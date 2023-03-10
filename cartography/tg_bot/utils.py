720ea42f67a68ad44d171701303c805adaeceb24
likeinlife <likeinlife48@yandex.ru>
2023-03-10 20:10:05 +0300

    Little reformat

--- cartography/tg_bot/utils.py
+++ cartography/tg_bot/utils.py
@@ -12,10 +12,7 @@ def make_float_list_from_str(data: str):
 def validate_coordinate(func: Callable):
 
     async def wrapper(message: Message, state: FSMContext):
-        if message.text is None:
-            await message.answer('Некорректные данные')
-            return
-        if re.fullmatch(r'([0-9]{1,3}) ?([0-9]{1,2})? ?([0-9]{1,2})?', message.text):
+        if re.fullmatch(r'([0-9]{1,3}) ?([0-9]{1,2})? ?([0-9]{1,2})?', str(message.text)):
             return await func(message, state)
         await message.answer('Некорректные данные')
 
