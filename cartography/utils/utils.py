5ea6ca3285e40e0efdb81e364b021ac64a074050
likeinlife <likeinlife48@yandex.ru>
2023-03-21 18:15:13 +0300

    Reformat

--- cartography/utils/utils.py
+++ cartography/utils/utils.py
@@ -9,7 +9,7 @@ def make_float_list_from_str(data: str):
     return list(map(float, data.strip(' ').split(' ')))
 
 
-def validate_coordinate(func: Callable):
+def validate_degrees_min_sec(func: Callable):
 
     async def wrapper(message: Message, state: FSMContext):
         if re.fullmatch(r'([0-9]{1,3}) ?([0-9]{1,2})? ?([0-9]{1,2})?', str(message.text)):
