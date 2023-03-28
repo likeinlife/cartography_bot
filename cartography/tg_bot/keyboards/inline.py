130a51a57b415b8403853315ddbbbfde6e444adf
likeinlife <likeinlife48@yandex.ru>
2023-03-28 17:43:04 +0300

    Minor changes

--- cartography/tg_bot/keyboards/inline.py
+++ cartography/tg_bot/keyboards/inline.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def get_buttons(escape_item: int):
+def get_help_buttons(escape_item: int):
     buttons = [{
         'text': 'Команды',
         'callback_data': 'help_commands'
@@ -23,12 +23,28 @@ def get_buttons(escape_item: int):
 def get_inline_help(escape: int) -> InlineKeyboardMarkup:
     inl_keyb = InlineKeyboardBuilder()
 
-    for button in get_buttons(escape):
+    for button in get_help_buttons(escape):
         inl_keyb.button(**button)
 
     if escape != -1:
         inl_keyb.button(text='Вернуться', callback_data='help_menu')
         inl_keyb.adjust(2, 1)
         return inl_keyb.as_markup()
+
+    return inl_keyb.as_markup()
+
+
+def get_inline_showKeyboard() -> InlineKeyboardMarkup:
+    inl_keyb = InlineKeyboardBuilder()
+
+    inl_keyb.button(text='Показать', callback_data='show_keyboard')
+
+    return inl_keyb.as_markup()
+
+
+def get_inline_hideKeyboard() -> InlineKeyboardMarkup:
+    inl_keyb = InlineKeyboardBuilder()
+
+    inl_keyb.button(text='Скрыть', callback_data='hide_keyboard')
 
     return inl_keyb.as_markup()
