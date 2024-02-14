ebdaa4986784803dcd92839a418d16cad37e2177
likeinlife <likeinlife@outlook.com>
2024-02-15 01:41:23 +0300

    feat(tests): more test

--- tests/test_coordinate_pair_actions.py
+++ tests/test_coordinate_pair_actions.py
@@ -4,33 +4,23 @@ import pytest
 from business.math_actions.coordinate_pair_actions import get_delta
 from domain.models import Coordinate, CoordinatePair
 
+from .coordinates_help import from_tuple
+
 
 @pytest.mark.parametrize(
     ("upper_bound", "lower_bound", "parts_number", "expected"),
     [
         (
-            CoordinatePair(latitude=Coordinate(degrees=_(10)), longitude=Coordinate(degrees=_(10))),
-            CoordinatePair(latitude=Coordinate(degrees=_(5)), longitude=Coordinate(degrees=_(5))),
+            CoordinatePair(latitude=from_tuple((10, 0, 0)), longitude=from_tuple((10, 0, 0))),
+            CoordinatePair(latitude=from_tuple((5, 0, 0)), longitude=from_tuple((5, 0, 0))),
             2,
-            CoordinatePair(
-                latitude=Coordinate(degrees=_(2), minutes=_(30)),
-                longitude=Coordinate(degrees=_(2), minutes=_(30)),
-            ),
+            CoordinatePair(latitude=from_tuple((2, 30, 0)), longitude=from_tuple((2, 30, 0))),
         ),
         (
-            CoordinatePair(
-                latitude=Coordinate(degrees=_(53), minutes=_(15)),
-                longitude=Coordinate(degrees=_(47), minutes=_(30)),
-            ),
-            CoordinatePair(
-                latitude=Coordinate(degrees=_(53), minutes=_(10)),
-                longitude=Coordinate(degrees=_(47), minutes=_(22), seconds=_(30)),
-            ),
+            CoordinatePair(latitude=from_tuple((53, 15, 0)), longitude=from_tuple((47, 30, 0))),
+            CoordinatePair(latitude=from_tuple((53, 10, 0)), longitude=from_tuple((47, 22, 30))),
             2,
-            CoordinatePair(
-                latitude=Coordinate(degrees=_(0), minutes=_(2), seconds=_(30)),
-                longitude=Coordinate(degrees=_(0), minutes=_(3), seconds=_(45)),
-            ),
+            CoordinatePair(latitude=from_tuple((0, 2, 30)), longitude=from_tuple((0, 3, 45))),
         ),
     ],
 )
